650e56716961aa1c373aa4586d1e58a36e0501ef
Dan Cowdry <medtc411@gmail.com>
2021-11-29 18:36:45 -0800

    committing

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -7,16 +7,23 @@ namespace CarTests
     public class CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
-      /*  [TestMethod]
-        public void EmptyTest()
+        /*  [TestMethod]
+          public void EmptyTest()
+          {
+              Assert.AreEqual(10,10,.001);
+          }*/
+
+        Car test_car;
+        [TestInitialize]
+        public void CreateCarObject()
         {
-            Assert.AreEqual(10,10,.001);
-        }*/
+            test_car = new Car("Toyota", "Prius", 10, 50);
+        }
         //TODO: constructor sets gasTankLevel properly
         [TestMethod]
         public void TestInitialGasTank()
         {
-            Car test_car = new Car("Toyota", "Prius", 10, 50);
+            /*Car test_car = new Car("Toyota", "Prius", 10, 50);*/
             Assert.AreEqual(10, test_car.GasTankLevel, .001);
         }
         //TODO: gasTankLevel is accurate after driving within tank range
